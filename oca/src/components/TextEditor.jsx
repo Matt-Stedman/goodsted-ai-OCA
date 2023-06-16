005f7ea3a41013e697d8e508122e91b2f373ae65
Matthew <matthew@sparkmate.co>
2023-06-16 13:37:12 +0200

    We now have a container box

--- oca/src/components/TextEditor.jsx
+++ oca/src/components/TextEditor.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useRef } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import MagicBox from "./MagicBox";
 
 const TextEditor = () => {
     const [blocks, setBlocks] = useState([""]);
+    const [showMagicBox, setShowMagicBox] = useState(false);
+    const [magicBoxPosition, setMagicBoxPosition] = useState({ x: 0, y: 0 });
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
     const quillRef = useRef();
     const editorRef = useRef();
@@ -24,6 +28,10 @@ const TextEditor = () => {
         }
     };
 
+    /**
+     * Helper function to get the block's index in a selection
+     * @param {} selection
+     */
     const handleGetSelectedBlock = (selection) => {
         // eslint-disable-next-line
         const { index, length } = selection;
@@ -38,11 +46,13 @@ const TextEditor = () => {
             }
             accumulatedLength++; // account for the newline character
         }
-
-        console.log("Selected Block Index:", blockIndex);
+        return blocks[blockIndex];
     };
 
-    const makeMagic = () => {
+    /**
+     * The Make Magic button's execution
+     */
+    const returnBlockAndSelection = () => {
         const selection = unprivilegedEditorRef.current.getSelection();
         const content = unprivilegedEditorRef.current.getText();
 
@@ -51,13 +61,47 @@ const TextEditor = () => {
 
             // Get the text context
             const selectedText = content.substr(index, length);
-            console.log(selectedText);
+            // console.log("Selected text: ", selectedText);
 
             // Get the block index
-            handleGetSelectedBlock(selection);
+            const selectedBlock = handleGetSelectedBlock(selection);
+            // console.log("Selected Block: ", selectedBlock);
+            return {
+                selectedText: selectedText,
+                selectedBlock: selectedBlock,
+                content: content,
+            };
         }
+        return {};
+    };
 
-        console.log(blocks);
+    /**
+     * Decide if we should show the button based on whether there is a selection of text or not.
+     */
+    const triggerButton = () => {
+        const selection = unprivilegedEditorRef.current.getSelection();
+        setShowMagicBox(false);
+        if (selection) {
+            if (selection.length) {
+                setShowMagicBox(true);
+                setMagicBoxPosition({ x: mousePosition.x, y: mousePosition.y });
+            }
+        }
+    };
+
+    /**
+     * Track the mouse movement ready for when we make a selection
+     */
+    const handleMouseMove = (event) => {
+        const { clientX, clientY } = event;
+
+        const editorElement = quillRef.current.editor.container;
+        const editorBounds = editorElement.getBoundingClientRect();
+        const buttonX = clientX - editorBounds.left;
+        const buttonY = clientY - editorBounds.top;
+        setMousePosition({ x: buttonX, y: buttonY });
+
+        //   setButtonPosition({ x: buttonX, y: buttonY });
     };
 
     React.useEffect(() => {
@@ -68,14 +112,19 @@ const TextEditor = () => {
     }, []);
 
     return (
-        <div>
+        <div style={{ display: "block" }} onMouseMove={handleMouseMove}>
             <ReactQuill
                 ref={quillRef}
                 // value={blocks.join("</p>")}
                 onChange={handleChange}
                 onKeyDown={handleKeyPress}
+                onChangeSelection={triggerButton}
             />
-            <button onClick={makeMagic}>Magic me!</button>
+            <MagicBox
+                magicBoxPosition={magicBoxPosition}
+                returnBlockAndSelection={returnBlockAndSelection}
+                showMagicBox={showMagicBox}
+            />
         </div>
     );
 };
